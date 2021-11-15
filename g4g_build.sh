500422b71c16ca71c250da07249968d4a9178cbf
Troy Lamerton <troy.lamerton@gmail.com>
2021-11-15 15:58:45 +0100

    Update g4g_build.sh

--- g4g_build.sh
+++ g4g_build.sh
@@ -4,4 +4,4 @@ OUTPUT_DIR="$REPO/CosPackages/BugsnagUnity/Packages/bugsnagunity/Runtime/Bugsnag
 ./gradlew :bugsnag-plugin-android-anr:assembleRelease
 ./gradlew :bugsnag-android-core:assembleRelease
 cp -f ./bugsnag-plugin-android-anr/build/outputs/aar/bugsnag-plugin-android-anr-release.aar $OUTPUT_DIR
-cp -f ./bugsnag-android-core/build/outputs/aar/bugsnag-android-core-release.aar $OUTPUT_DIR
+cp -f ./bugsnag-android-core/build/outputs/aar/bugsnag-android-core-release.aar $OUTPUT_DIR/bugsnag-android-release.aar
